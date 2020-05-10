76fd42160f99d63a8b5af8314d6edc7e92683f39
yliu333 <yihaoliu007@gmail.com>
2020-05-09 23:43:13 -0400

    heat map done. UDP done. Tested worked

---
+++ clientUnity/Assets/Scripts/MeshCreater.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter))]
+public class MeshCreater : MonoBehaviour
+{
+
+    [System.Serializable]
+    public class ImplantMesh
+    {
+        public ImplantMeshJson GetImplantMesh;
+    }
+
+    [System.Serializable]
+    public class ImplantMeshJson
+    {
+        public float[] vertices;
+        public int[] triangles;
+        public float[] color;
+    }
+    private UDPClient udpClient;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        udpClient = GetComponent<UDPClient>();
+
+        //Mesh mesh = new Mesh();
+        //GetComponent<MeshFilter>().mesh = mesh;
+        //Vector3[] verticesTest;
+        //int[] trianglesTest;
+        //verticesTest = new Vector3[]
+        //{
+        //        new Vector3 (0,0,0),
+        //        new Vector3 (0,0,1),
+        //        new Vector3 (1,0,0),
+        //        new Vector3 (1,0,1)
+        //};
+        //trianglesTest = new int[]
+        //{
+        //        0,1,2,
+        //        1,3,2
+        //};
+
+        //mesh.Clear();
+        //mesh.vertices = verticesTest;
+        //mesh.triangles = trianglesTest;
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        string message = "";
+
+        message = udpClient.GetLatestUDPPacket();
+        
+        ImplantMesh m = JsonUtility.FromJson<ImplantMesh>(message);
+
+        if (message != "")
+        {
+            Mesh mesh = new Mesh();
+            GetComponent<MeshFilter>().mesh = mesh;
+            Debug.Log(m.GetImplantMesh.triangles);
+            Vector3[] vertices = new Vector3[m.GetImplantMesh.vertices.Length / 3];
+
+            for (int i = 0; i < m.GetImplantMesh.vertices.Length / 3; i++)
+                vertices[i] = new Vector3(m.GetImplantMesh.vertices[3 * i], m.GetImplantMesh.vertices[3 * i + 1], m.GetImplantMesh.vertices[3 * i + 2]);
+
+            mesh.Clear();
+            mesh.vertices = vertices;
+            mesh.triangles = m.GetImplantMesh.triangles;
+            Color[] colors = new Color[mesh.vertices.Length];
+            for (int i = 0; i < mesh.vertices.Length; i++)
+                colors[i] = colors[i] = Color.Lerp(Color.blue, Color.red, m.GetImplantMesh.color[i]);
+            mesh.colors = colors;
+            mesh.RecalculateNormals();
+
+        }
+
+
+
+
+    }
+}
--- serverPython/main.py
+++ serverPython/main.py
@@ -15,6 +15,9 @@ from dataReadHandling import coordAndIndices2Triangles
 from util.octree.createSphereAndTriangles import createSpheres
 from util.octree.BoundingBoxTreeNode import BoundingBoxTreeNode
 from util.icp import octreeSearchMatch
+import socket
+import json
+import time
 
 loadCalibratedPointer = True
 loadCalibratedHemo = True
@@ -63,7 +66,7 @@ if loadCollectedImplant:
 else:
     print('press enter key to collect points on the implant')
     input()
-    tImplant = digitizationRightHand(1000, 16, pCalHemo, pCalPointer, [1,2]) 
+    tImplant = digitizationRightHand(200, 10, pCalHemo, pCalPointer, [1,2]) 
     np.savetxt('data/out/digitizedImplant.txt',tImplant)
 
 # 2.2 Create implant surface
@@ -75,9 +78,9 @@ implantCoord, implantIndices = convertPlySur('data/implantSurface.ply')
 #############################################################################
 # 3. Receive a transformation of implant from virtual space (note now both implant and skull should be x-flipped)
 quatSkull = np.array([-0.7071068, 0.0000000, 0.0000000, 0.7071068])
-posSkull = 1000 * np.array([-0.0330000, -0.0158000, -0.0580000])
+posSkull = 1000 * np.array([-0.0330000, -0.0658000, -0.0580000])
 quatImplant = np.array([-0.4427488, -0.0442963, 0.1601198, 0.8811204])
-posImplant = 1000 * np.array([-0.0209000, 0.1048000, -0.0153000])
+posImplant = 1000 * np.array([-0.0209000, 0.0522000, -0.0153000])
 
 rSkull = Rotation.from_quat(quatSkull)
 rImplant = Rotation.from_quat(quatImplant)
@@ -89,25 +92,37 @@ print('Creating octree ...')
 octree = BoundingBoxTreeNode(spheres)
 print('Data meshing completed, and octree created.')
 
-# 3.2 Convert the implant model to left handedness (flip x) and find the center of triangle
+# 3.2 Convert the implant model to left handedness (flip x) 
 implantCoordLeft = implantCoord
 implantCoordLeft[:,0] = -implantCoordLeft[:,0] 
-tempSph = createSpheres(coordAndIndices2Triangles(implantCoordLeft, implantIndices))
-targetPoints = np.empty([len(tempSph), 3])
-for i in range(len(tempSph)):
-    targetPoints[i] = tempSph[i].center
+
 
 # 3.3 Transform implant model to the moved position
-for i in range(targetPoints.shape[0]):
-    targetPoints[i] = np.matmul(rImplant.as_matrix(), targetPoints[i].reshape((3,1))).reshape((3,))+posImplant
+for i in range(implantCoordLeft.shape[0]):
+    implantCoordLeft[i] = np.matmul(rImplant.as_matrix(), implantCoordLeft[i].reshape((3,1))).reshape((3,))+posImplant
 
 # 3.4 Match the points to the surface and calculate distance
 print('start searching ...')
-cOut, dist = octreeSearchMatch(targetPoints, octree)
+cOut, dist = octreeSearchMatch(implantCoordLeft, octree)
 
 #############################################################################
 # 4. Send heat map data and the point pair on the skull
 
+# unity /1000
+implantCoordLeftSend = implantCoordLeft/1000
+implantIndicesSend = implantIndices.reshape(-1)
+# convert distance into 0~1 range
+distColor = (dist-min(dist))/(max(dist)-min(dist))
+
+udpIP = 'localhost'
+udpPort = 8051
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+message = json.dumps({'GetImplantMesh': {'vertices': implantCoordLeftSend.reshape(-1).tolist(), 'triangles': implantIndicesSend.tolist(), 'color': distColor.tolist()}})
+# message = json.dumps({'GetImplantMesh': {'vertices': (0.1*np.array([[0,0.3,0],[0,0,1],[1,0.5,0],[1,0.5,1],[2,0.5,1],[3,0.5,3]])).reshape(-1).tolist(), 'triangles': [0,1,2,1,3,2,2,3,4,3,4,5], 'color': [0.1,1,0.3,1]}})
+for i in range(3000):
+    sock.sendto(message.encode('utf-8'), (udpIP, udpPort))
+    time.sleep(5)
+sock.close()
 
 #############################################################################
 # 5. Overlay heat map to implant (need collect right and left hand implant at the same time)
